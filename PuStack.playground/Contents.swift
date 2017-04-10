1f13f772dbfff958038ba05a4c492f6a5a4bb780
Neha Sharma <nsharma5@illinois.edu>
2017-04-10 15:49:49 -0500

    Aakanksha's commits that are running into conflicts over stupid files...
    # Conflicts:
    #
    PuStack.playground/playground.xcworkspace/xcuserdata/apple.xcuserdatad/U
    serInterfaceState.xcuserstate

--- PuStack.playground/Contents.swift
+++ PuStack.playground/Contents.swift
@@ -1,11 +1,17 @@
 //: Playground - noun: a place where people can play
+
 import UIKit
+import XCPlayground
 import PlaygroundSupport
 
-let container = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 300))
-let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+var str = "Hello, playground"
+let color = UIColor (red: 1 , green: 1 , blue: 0 , alpha: 0 )
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
 view.backgroundColor = UIColor.red
-container.addSubview(view)
-
-PlaygroundPage.current.liveView = container
+view.frame = CGRect (x: 0 ,y: 0 ,width: 200 ,height: 200 )
 PlaygroundPage.current.needsIndefiniteExecution = true
+PlaygroundPage.current.liveView = view
+let label = UILabel (frame: CGRect (x: 5 , y: 5 , width: 200 , height: 20 ))
+label.text = str
+view.addSubview(label)
+
